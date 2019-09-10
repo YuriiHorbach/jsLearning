728255e7357b53e00fe72ed244f97797426aebd8
southl <gyrii84@gmail.com>
2019-09-10 17:24:28 +0300

    add unit 5

--- cycle-1/app/js/script.js
+++ cycle-1/app/js/script.js
@@ -1,14 +1,18 @@
+// for
+//while
+
 let div = document.querySelectorAll('.one');
+console.log(div);
+//div.style.background = 'red';
 
-// for (let i = 0; i < div.length; i++) {
-//     div[i].style.background = 'red';
+// for (let i = 0; i < div.length; i = i + 1) {
 //     console.log(div[i]);
+//     div[i].style.background = 'red';
 //     div[i].onclick = two;
-
 // }
 
 // function two() {
-//     console.log('work!!');
+//     console.log('work!!!');
 // }
 
 // let b = document.getElementsByClassName('one');
@@ -18,28 +22,23 @@ let div = document.querySelectorAll('.one');
 
 // for (let i = 0; i < b.length; i++) {
 //     b[i].style.border = '3px solid black';
-// }
-let out2 = document.querySelector('.out2');
-let btn = document.querySelector('button').onclick = () => {
+// } 
+
+document.querySelector('button').onclick = () => {
     let r = document.querySelectorAll('input[type="radio"]');
     console.log(r);
     for (let i = 0; i < r.length; i++) {
         if (r[i].checked) {
             console.log(r[i].value);
-            out2.innerHTML = r[i].value;
         }
     }
 }
-let out = ' ';
+
+let out = '';
 for (let i = 0; i < 10; i++) {
-    if (i == 6) {
-        continue;
-    }
+
+    if (i == 6) continue;
     out += i + ' ';
-    // if (i == 6) {
-    //     break;
-    // }
-
+    // if (i == 6) break;
 }
-document.querySelector("#out").innerHTML = out;
-
+document.querySelector('#out').innerHTML = out;
---
+++ unit_5/script.js
@@ -0,0 +1,156 @@
+
+/*
+Task 1. Выведите в консоль с помощью цикла числа от 1 до 10.
+*/
+for (let i = 1; i < 11; i++) {
+    console.log(i);
+}
+
+//task_2
+
+let out_t2 = document.querySelector('.out_t2');
+
+for (let i = 1; i < 11; i++) {
+    out_t2.innerHTML += i + ' ';
+}
+
+//task_3
+
+let out_t3 = document.querySelector('.out_t3');
+
+for (let i = 10; i >= 0; i--) {
+    out_t3.innerHTML += i + ' ';
+}
+
+//task_4
+
+let out_t4 = document.querySelector('.out_t4');
+
+for (let i = 0; i <= 10; i += 2) {
+    out_t4.innerHTML += i + ' ';
+}
+
+//task_5
+
+let out_t5 = document.querySelector('.out_t5');
+
+for (let i = 21; i >= 0; i -= 3) {
+    out_t5.innerHTML += i + ' ';
+}
+
+//task_6
+
+let out_t6 = document.querySelector('.out_t6');
+
+for (let i = 0; i <= 5; i++ ) {
+    out_t6.innerHTML += '****** <br>';
+}
+
+//task_7
+let inp_t7 = document.querySelector('.inp_t7');
+let out_t7 = document.querySelector('.out_t7');
+let btn_t7 = document.querySelector('.btn_t7');
+
+btn_t7.onclick = () => {
+    for(let i = inp_t7.value; i >= 0; i--){
+        out_t7.innerHTML += i + ' ';
+    }
+}
+
+//task_8
+let inp_t81 = document.querySelector('.inp_t81');
+let inp_t82 = document.querySelector('.inp_t82');
+let out_t8 = document.querySelector('.out_t8');
+let btn_t8 = document.querySelector('.btn_t8');
+
+btn_t8.onclick = () => {
+    for(let i = inp_t81.value; i <= inp_t82.value ; i++){
+        out_t8.innerHTML += i + ' ';
+    }
+}
+
+//task_9
+let inp_t91 = document.querySelector('.inp_t91');
+let inp_t92 = document.querySelector('.inp_t92');
+let out_t9 = document.querySelector('.out_t9');
+let btn_t9 = document.querySelector('.btn_t9');
+
+btn_t9.onclick = () => {
+
+    if (inp_t92.value > inp_t91.value){
+        for(let i = inp_t91.value; i <= inp_t92.value ; i++){
+            out_t9.innerHTML += i + ' ';
+        }
+    }
+    else{
+        alert('Error');
+    }
+}
+
+//task_10
+let out_t10 = document.querySelector('.out_t10');
+    for(let i = 1901; i <= 1950; i ++){
+        if(i % 2==0){
+            out_t10.innerHTML += i + " ";
+        }
+    }
+
+//task_11
+let out_t11 = document.querySelector('.out_t11');
+let one_t11 = document.querySelectorAll('.one');
+out_t11.innerHTML = 'Quantity of div.one: ' + one_t11.length;
+
+//task_12
+let btn_t12 = document.querySelector('.btn_t12');
+  
+function backColor(){
+    for(let i = 0; i < one_t11.length; i++){
+        one_t11[i].style.background = 'yellow';
+    }
+}
+
+btn_t12.onclick = backColor;
+
+//task_13
+let btn_t13 = document.querySelector('.btn_t13');
+  
+function getAllInfo(){
+    for(let i = 0; i < one_t11.length; i++){
+        console.log(one_t11[i].innerHTML);
+    }
+}
+
+btn_t13.onclick = getAllInfo;
+
+//task_14
+let btn_t14 = document.querySelector('.btn_t14');
+let inputs = document.querySelectorAll('input[type="text"]');
+  
+function setPlaceHolder(){
+    for(let i = 0; i < inputs.length; i++){
+        inputs[i].placeholder = 'Введите данные';
+    }
+}
+
+btn_t14.onclick = setPlaceHolder;
+
+//task_15
+let btn_t15 = document.querySelector('.btn_t15');
+let inputsAll = document.querySelectorAll('input[type="text"]');
+  
+function getQuantity(){
+    console.log('Quantity of inputs ' + inputsAll.length);
+}
+
+btn_t15.onclick = getQuantity;
+
+//task_16
+let radioInp = document.querySelectorAll('input[type="radio"]');
+let out_t16 = document.querySelector('.out_t16');
+  
+for(let i = 0; i < radioInp.length; i++){
+    if(radioInp[i].checked){
+        out_t16.innerHTML = radioInp[i].value;
+    }
+}
+
