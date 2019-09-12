106977eb936e7d829c6f5dcc4bd8b17088ffe441
southl <gyrii84@gmail.com>
2019-09-12 17:19:54 +0300

    add hw unit 6 before task7

--- unit_6/script.js
+++ unit_6/script.js
@@ -38,19 +38,57 @@ for (let i = 0; i < 3; i++) {
 let out_t4 = document.querySelector('.out_t4');
 
 for (let i = 0; i < 3; i++) {
-    for (let k = 1; k < 7; k++) {
-        if(k % 2 == 0){
+    for (let k = -2; k < 4; k++) {
+        if(k % 3 == 0){
+            out_t4.innerHTML += 'x';
+        } else if(k % 2 == 0){
             out_t4.innerHTML += 1;
         }
-        // else if(k % 3 == 0){
-        //     out_t4.innerHTML += 'x';
-        // }
         else{
             out_t4.innerHTML += 0;
         }
     }
-    for(let j = 1; j < 7; j +=3){
-        out_t4.innerHTML += 'x';
-    }
     out_t4.innerHTML += '<br> ';
 }
+
+//task5
+let out_t5 = document.querySelector('.out_t5');
+
+let count = 1;
+for (let i = 0; i < 3; i++) {
+    for (let k = 0; k < count; k++) {
+        out_t5.innerHTML += '*';
+    }
+    out_t5.innerHTML += '<br> ';
+    count++;
+}
+
+//task6
+let out_t6 = document.querySelector('.out_t6');
+
+let count2 = 5;
+for (let i = 0; i < 5; i++) {
+    for (let k = count2; k > 0; k--) {
+        out_t6.innerHTML += '*';
+    }
+    out_t6.innerHTML += '<br> ';
+    count2--;
+}
+
+//task7
+let out_t7 = document.querySelector('.out_t7');
+let sp = '&nbsp';
+
+for (let i = 0; i < 3; i++) {
+    console.log(sp);
+    for(let k = 0; k < 6; k++){
+      out_t7.innerHTML += '*'; 
+      console.log('*');
+    }
+    sp+=sp;
+    console.log(out_t7);
+    out_t7.innerHTML +='<br>';
+    out_t7.innerHTML +=sp;
+}
+
+
