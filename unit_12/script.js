0de93251465fb45f779ea2558c3edbb24b418f42
gyrii84@gmail.com <gyrii84@gmail.com>
2019-10-04 22:27:59 +0300

    add unit 12 task 12 doing now

--- unit_12/script.js
+++ unit_12/script.js
@@ -53,3 +53,138 @@ Task 4.
 let out12_4 = document.querySelector('.out-12-4');
 
 out12_4.innerHTML += a[2];
+
+/* 
+Task 5.
+Выведите на страницу только первые элементы вложенных массивов массива a.
+*/
+
+let out12_5 = document.querySelector('.out-12-5');
+
+
+for(let i = 0; i < a.length; i++){
+    for(let k = 0 ; k < a[i].length; k++){
+    }
+    out12_5.innerHTML += a[i][0] + ' ';
+    out12_5.innerHTML += '<br>';
+}
+
+/* 
+Task 6.
+Выведите на страницу только четные вложенные массивы массива a.
+*/
+
+let out12_6 = document.querySelector('.out-12-6');
+
+for(let i = 0; i < a.length; i++){
+    for(let k = 0 ; k < a[i].length; k++){
+        if(i % 2 == 0){
+            out12_6.innerHTML += a[i][k] + ' ';
+    
+        }
+    }
+     out12_6.innerHTML += '<br>';
+}
+
+/* 
+Task 7.
+Выведите на страницу только числа из массива a.
+*/
+
+let out12_7 = document.querySelector('.out-12-7');
+
+for(let i = 0; i < a.length; i++){
+    for(let k = 0 ; k < a[i].length; k++){
+        if(!isNaN(a[i][k])){
+            out12_7.innerHTML += a[i][k] + ' ';
+        }
+    }
+     out12_7.innerHTML += '<br>';
+}
+/* 
+Task 8.
+Выведите на страницу только числа из массива a.
+*/
+
+let out12_8 = document.querySelector('.out-12-8');
+
+for(let i = 0; i < a.length; i++){
+    for(let k = 0 ; k < a[i].length; k++){
+       
+    }
+    out12_8.innerHTML += a[i].length;
+    out12_8.innerHTML += '<br>';
+}
+
+/* 
+Task 9.
+Выведите на страницу элементы массива a в обратном порядке, т.е.
+    9 8 7 f e ...
+*/
+
+let out12_9 = document.querySelector('.out-12-9');
+
+for(let i = a.length - 1; i > 0 ; i--){
+    for(let k = a[i].length - 1 ; k >= 0; k--){
+        out12_9.innerHTML += a[i][k] + ' ';
+    }
+}
+
+/* 
+Task 10.
+        Выведите на страницу элементы массива a, причем вложенные массивы должны выводиться в обратном порядке:
+            3 2 1
+            c b a
+            6 5 4   
+*/
+
+let out12_10 = document.querySelector('.out-12-10');
+
+for(let i = 0; i < a.length; i++){
+    for(let k = a[i].length - 1 ; k >= 0; k--){
+        out12_10.innerHTML += a[i][k] + ' ';
+    }
+    out12_10.innerHTML += '<br>';
+}
+
+/* 
+Task 11.
+        Создайте массив шахматную доску. Белые - 0, черные - 1. Выведите на страницу.
+*/
+
+let out12_11 = document.querySelector('.out-12-11');
+
+const b = [
+    [0,1,0,1,0,1,0,1],
+    [1,0,1,0,1,0,1,0],
+    [0,1,0,1,0,1,0,1],
+    [1,0,1,0,1,0,1,0],
+    [0,1,0,1,0,1,0,1],
+    [1,0,1,0,1,0,1,0],
+    [0,1,0,1,0,1,0,1],
+    [1,0,1,0,1,0,1,0]
+]
+
+for(let i = 0; i < b.length; i++){
+    for(let k = 0; k < b[i].length; k++){
+        out12_11.innerHTML += b[i][k] + ' ';
+    }
+    out12_11.innerHTML += '<br>';
+}
+/* 
+Task 12.
+Создайте массив шахматную доску. Белые - 0, черные - 1. Напишите функцию, которая выводит данный массив в виде шахматной доски - блоки div в нужном порядке, закранные цветом.
+*/
+
+let out12_12 = document.querySelector('.out-12-12');
+
+for(let i = 0; i < b.length; i++){
+    for(let k = 0; k < b[i].length; k++){
+        out12_12.innerHTML += b[i][k] + ' ';
+    }
+    out12_12.innerHTML += '<br>';
+}
+
+function chessBoard(arr){
+    
+}
