a9a49a89729a37079ccf1aeb5f2bbc0ca2ec5c0b
southl <gyrii84@gmail.com>
2019-10-19 12:45:49 +0300

    unit 14 doing

---
+++ unit_14/script.js
@@ -0,0 +1,17 @@
+console.log(cities.length);
+
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=5912ee9eb422482a793f8e74475f1d7a`)
+    .then(function (resp) { return resp.json() })
+    .then(function (data) {
+        console.log(data);
+        document.querySelector('.package-name').textContent = data.name;
+        document.querySelector('.price').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+        document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
+        //https://openweathermap.org/img/wn/02d@2x.png
+        document.querySelector('.features li').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+    })
+    .catch(function () {
+        // catch any errors
+    });
+
+ 
