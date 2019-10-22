5de9750100842c4d564c355579ef0f1c7e1befbd
southl <gyrii84@gmail.com>
2019-10-22 12:51:02 +0300

    unit 14 doing do not know how get value from select option

--- unit_14/script.js
+++ unit_14/script.js
@@ -3,6 +3,7 @@
     let out14 = document.querySelector('.out14');
     let sel14 = document.querySelector('.sel14');
     
+    
 
 
     for(let i = 0; i < cleanCountriesArr.length; i++){
@@ -16,16 +17,17 @@
             return elem.value;
     }
 
-    function getValue2(elem){
-        select = document.querySelector();
-        return elem.value;
-}
-
+    // function selectValue(sel) {
+    //     var selElem = sel;
+    //     var selElemVal = selElem.options[selElem.selectedIndex].value;
+    //     return selElemVal;
+    // }
     
+   let arrAllCountry = [];
    
-    
+   let val14 = ' ';
     country.onchange = () =>{
-        let val14 = ' ';
+        
         let countryOption = '';
         // countryOption =  getValue(country);
         for(let i = 0; i < cities.length; i ++){
@@ -34,50 +36,80 @@
                 if(cities[i][key] == getValue(country)){
 
                     let arrCountry = {};
-                    let key = getValue(country);
+                    let key = 'country';
+                    let id = 'id';
                     //  console.log(key);
                     // for(let key in arrCountry){
                         arrCountry[key] = cities[i]['name'];
-                        // console.log(arrCountry);
+                        arrCountry[id] = cities[i]['id'];
+                        console.log(arrCountry);
                         // arrCountry[key].id = cities[i]['id'];
-                        val14 += `<option value = "">${arrCountry[key]}</option>`;
-                        //  console.log(val14);
+                        val14 += `<option value = "${arrCountry[id]}">${arrCountry[key]}</option>`;
+                        console.log(val14);
                         // console.log(arrCountry[keyCountry].name);
-                    // }
+                        console.log(arrCountry[key]);
+                        arrAllCountry.push(arrCountry);
+                        
                 }
+                
+                
             }
+           
         }
+
+        let myVar = document.querySelector('.sel14');
+        // let myVarValue = myVar.options[myVar.selectedIndex].text;
+        // console.log(myVarValue);
+
         sel14.innerHTML = val14;
 
-        let selOption = document.querySelector('.sel14 option');
-        console.log(selOption);
-        let valnew = selOption.innerHTML;
+        // if(){}
         
-     
-        let btn = document.querySelector('.btn');
+        console.log(arrAllCountry);
 
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${valnew}&appid=70e1ed322b02acbc57d443dd91065f3e`)
-        .then(function (resp) { return resp.json() })
-        .then(function (data) {
-            console.log(data);
-            document.querySelector('.package-name').textContent = data.name;
-            document.querySelector('.price').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
-            document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
-            //https://openweathermap.org/img/wn/02d@2x.png
-            document.querySelector('.features li').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-        })
-        .catch(function () {
-            // catch any errors
-        });
+        for(let i = 0; i < arrAllCountry.length; i++){
+            for(key in arrAllCountry[i]){
+                // if(key == ){
 
-        btn.onclick = ()=>{
-            
-            valnew = '';
+                // }
+            }
         }
-
+       
     }
-    
 
+    // if()
+    
+    // let optionValue = document.querySelector('.sel14 option');
+   
+
+   
+           
+
+        
+
+           
+
+    // }
+
+
+    
+    
+    
+ 
+    // fetch(`http://api.openweathermap.org/data/2.5/weather?q=${valnew}&appid=5912ee9eb422482a793f8e74475f1d7a`)
+    // .then(function (resp) { return resp.json() })
+    // .then(function (data) {
+    //     console.log(data);
+    //     document.querySelector('.package-name').textContent = data.name;
+    //     document.querySelector('.price').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+    //     document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
+    //     //https://openweathermap.org/img/wn/02d@2x.png
+    //     document.querySelector('.features li').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+    // })
+    // .catch(function () {
+    //     // catch any errors
+    // });
+    
 
     
 
