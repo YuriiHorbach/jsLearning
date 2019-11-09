ebedbafca470d2bc10743f603f8a1752a43a9b40
southl <gyrii84@gmail.com>
2019-11-09 11:42:05 +0200

    add unit 18 add 8

--- unit_18/unit_18.js
+++ unit_18/unit_18.js
@@ -92,7 +92,7 @@ document.querySelector('.b-6').onclick = t6;
 
 function t7() {
     let  str7 = 'hello world hi mazai';
-    let a7_res = str7.split(' ');
+    a7_res = str7.split(' ');
     document.querySelector('.out-7').innerHTML = a7_res;
 }
 
@@ -102,7 +102,9 @@ document.querySelector('.b-7').onclick = t7;
 /*  Дан массив a8 = [1,2,66,77,15] - преобразуйте ее в строку. Разделитель - дефис. Результат присвойте a8_res. Запускаться решение должно при вызове функции t8. */
 
 function t8() {
-
+    let a8 = [1,2,66,77,15];
+    a8_res = a8.join('-');
+    document.querySelector('.out-8').innerHTML = a8_res;
 }
 
 document.querySelector('.b-8').onclick = t8;
